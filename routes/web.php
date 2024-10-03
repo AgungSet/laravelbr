5f1bf6d5820d4a60464dd1a9e79a76a95176a492
AgungSet <116327716+AgungSet@users.noreply.github.com>
2024-10-03 16:11:13 +0700

    nav

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,25 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/menu', function () {
-    return view('menu');
-})->middleware(['auth', 'verified'])->name('menu');
+Route::get('/produk', function () {
+    return view('produk');
+})->middleware(['auth', 'verified'])->name('produk');
+
+Route::get('/kategori', function () {
+    return view('kategori');
+})->middleware(['auth', 'verified'])->name('kategori');
+
+Route::get('/transaksi', function () {
+    return view('transaksi');
+})->middleware(['auth', 'verified'])->name('transaksi');
+
+Route::get('/member', function () {
+    return view('member');
+})->middleware(['auth', 'verified'])->name('member');
+
+Route::get('/pesanan', function () {
+    return view('pesanan');
+})->middleware(['auth', 'verified'])->name('pesanan');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
