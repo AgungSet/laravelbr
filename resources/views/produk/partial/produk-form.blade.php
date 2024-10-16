d6bf82319df28bfe29feca59ff206e1c8588ff27
AgungSet <116327716+AgungSet@users.noreply.github.com>
2024-10-16 18:44:10 +0700

    7

--- resources/views/produk/partial/produk-form.blade.php
+++ resources/views/produk/partial/produk-form.blade.php
@@ -2,13 +2,13 @@
     <div class="flex flex-col mt-4 gap-6">
         <div class="flex flex-col md:flex-row flex-wrap gap-4">
             <div class="flex-1">
-                <label for="nama_kategori" :value="__('Nama kategori')"> Nama Kategori</label>
-                <input type="text" id="nama_kategori" name="nama_kategori" value="{{ old('nama_kategori', optional($kategori ?? null)->nama_kategori) }}" class="mt-1 block w-full" />
+                <label for="produk" :value="__('Produk')"> Produk</label>
+                <input type="text" id="nama_produk" name="nama_produk" value="{{ old('nama_produk', optional($produk ?? null)->kategori) }}" class="mt-1 block w-full" />
             </div>
         </div>
         <div>
             <label for="deskripsi">deskripsi</label>
-            <textarea id="deskripsi" name="deskripsi" type="text" class="mt-1 block w-full">{{ old('deskripsi', optional($kategori ?? null)->deskripsi ? $kategori->deskripsi : '') }}</textarea>
+            <textarea id="deskripsi" name="deskripsi" type="text" class="mt-1 block w-full">{{ old('deskripsi', optional($produk ?? null)->deskripsi ? $produk->deskripsi : '') }}</textarea>
         </div>
     </div>
 </section>
